58cd7a6a9c41831cd2823204af3137095010a896
ryankayne <coope1rk@gmail.com>
2019-11-14 17:39:56 -0500

    Project setup now lol

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import React from 'react';
+import { withFormik, Form, Field } from "formik";
+import axios from "axios";
+import * as Yup from "yup";
 import logo from './logo.svg';
 import './App.css';
+import NewForm from "./NewForm";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <NewForm />
       </header>
     </div>
   );
---
+++ src/NewForm.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { Form } from "formik";
+
+const NewForm = "test";
+
+export default NewForm;
